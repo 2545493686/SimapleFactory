1d7cfbd53f6c01950d1a1dae61b9c76309636e33
杨东雄 <2545493686@qq.com>
2019-06-13 18:20:18 +0800

    添加项目文件。

---
+++ SimapleQQ/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace SimapleQQ
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.nameTextbox = new System.Windows.Forms.TextBox();
+            this.passwordTextbox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(75, 66);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // nameTextbox
+            // 
+            this.nameTextbox.Location = new System.Drawing.Point(27, 12);
+            this.nameTextbox.Name = "nameTextbox";
+            this.nameTextbox.Size = new System.Drawing.Size(175, 21);
+            this.nameTextbox.TabIndex = 1;
+            // 
+            // passwordTextbox
+            // 
+            this.passwordTextbox.Location = new System.Drawing.Point(27, 39);
+            this.passwordTextbox.Name = "passwordTextbox";
+            this.passwordTextbox.Size = new System.Drawing.Size(175, 21);
+            this.passwordTextbox.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(225, 99);
+            this.Controls.Add(this.passwordTextbox);
+            this.Controls.Add(this.nameTextbox);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "SampleQQ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox nameTextbox;
+        private System.Windows.Forms.TextBox passwordTextbox;
+    }
+}
+
---
+++ SimapleQQ/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimapleQQ
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            IVerify verify = VerifierFactory.GetVerifier("normal");
+            MessageBox.Show(verify.Verify(nameTextbox.Text, passwordTextbox.Text) ? "登录成功！" : "登录失败！");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+    }
+
+    public class VerifierFactory
+    {
+        public static IVerify GetVerifier(string type)
+        {
+            switch (type)
+            {
+                case "normal":
+                    return new Verifier();
+                case "file":
+                    return new FileVerifier();
+                case "net":
+                    return new NetVerifier();
+                default:
+                    throw new Exception("无效的字符串！");
+            }
+        }
+    }
+
+    public interface IVerify
+    {
+        bool Verify(string name, string password);
+    }
+
+    public class NetVerifier : IVerify
+    {
+        public bool Verify(string name, string password)
+        {
+            //.....
+            return false;
+        }
+    }
+
+    public class FileVerifier : IVerify
+    {
+        public bool Verify(string name, string password)
+        {
+            List<VerifyData> verifyDatas = ReadVerifyDatas();
+
+            for (int i = 0; i < verifyDatas.Count; i++)
+            {
+                if (verifyDatas[i].name == name && verifyDatas[i].password == password)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private static List<VerifyData> ReadVerifyDatas()
+        {
+            string datasText = File.ReadAllText(@"C:\Users\杨东雄\Desktop\VerifyDatas.txt");
+            string[] datas = datasText.Split('\n');
+            List<VerifyData> verifyDatas = new List<VerifyData>();
+            foreach (var item in datas)
+            {
+                verifyDatas.Add(new VerifyData
+                {
+                    name = item.Split(' ')[0],
+                    password = item.Split(' ')[1],
+                });
+            }
+
+            return verifyDatas;
+        }
+    }
+
+    public struct VerifyData
+    {
+        public string name;
+        public string password;
+    }
+
+    public class Verifier : IVerify
+    {
+        VerifyData[] verifyDatas =
+        {
+            new VerifyData { name = "name", password = "123456" },
+            new VerifyData { name = "xml", password = "123456" },
+        };
+
+
+        public bool Verify(string name, string password)
+        {
+            for (int i = 0; i < verifyDatas.Length; i++)
+            {
+                if (verifyDatas[i].name == name && verifyDatas[i].password == password)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
